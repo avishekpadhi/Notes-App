77564cc5b8b3ede281aceefe230886c04ac15859
Abhishek <abhishekpadhi69@gmail.com>
2022-10-12 16:39:27 +0530

    tried to add Important

--- notes.js
+++ notes.js
@@ -1,9 +1,18 @@
 console.log("Welcome to notes app. This is app.js");
 showNotes();
 
+
+let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  } 
+
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
+  
   let addTxt = document.getElementById("addTxt");
   let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
@@ -20,9 +29,12 @@ addBtn.addEventListener("click", function(e) {
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
   addTitle.value = "";
-//   console.log(notesObj);
+  console.log(notesObj); 
   showNotes();
+
+
 });
+
 
 // Function to show elements from localStorage
 function showNotes() {
@@ -34,13 +46,15 @@ function showNotes() {
   }
   let html = "";
   notesObj.forEach(function(element, index) {
+
+
+    // console.log(index)
     html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div id="div${index}"class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">${element.title}</h5>
                         <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                        <button id="imp${index}"onclick="important(this.id)" class="btn btn-primary">Important</button>
                     </div>
                 </div>`;
   });
@@ -59,12 +73,7 @@ function showNotes() {
 function deleteNote(index) {
 //   console.log("I am deleting", index);
 
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
+  
 
   notesObj.splice(index, 1);
   localStorage.setItem("notes", JSON.stringify(notesObj));
@@ -90,7 +99,22 @@ search.addEventListener("input", function(){
     })
 })
 function important(index){
-  console.log("This is very important");
+  // console.log(index);
+
+  let classnote=document.getElementsByClassName("noteCard")  
+  console.log(classnote)
+  let requirednote=classnote.getElementById
+
+  // let notes = localStorage.getItem("notes");
+  // if (notes == null) {
+  //   notesObj = [];
+  // } else {
+  //   notesObj = JSON.parse(notes);
+  // } 
+
+
+  // notesObj[this.index].classList.add("important");
+  
 }
 
 
